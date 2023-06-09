492803e1c40de42f9fa3248c046d92714930aa0a
braifz <braifz.dev@gmail.com>
2023-06-08 22:17:07 -0300

    added publicKeys balances

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+
 const app = express();
-const cors = require("cors");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "02dacc7a19ae90de06259527e2f42ad08d2f7bca73409f3cdae72fea8764fefa3f": 100,
+  "030fa5df60bb1e1683bc96bef960ac62cc4de595e9435abcb60c1c93b90abb9db1": 50,
+  "032ce306552085d18d714725930b69976e4936142b08ec6f286966abb7c0e8b6da": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
