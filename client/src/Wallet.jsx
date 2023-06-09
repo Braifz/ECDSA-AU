41cbc1e334731da34a2704ebb9592194694176bf
braifz <braifz.dev@gmail.com>
2023-06-08 22:16:13 -0300

    added ethereum cryptography and privateKeys input

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,19 @@
 import server from "./server";
+import {secp256k1} from "ethereum-cryptography/secp256k1"
+import {toHex, utf8ToBytes} from "ethereum-cryptography/utils"
+import {sha256} from "ethereum-cryptography/sha256.js"
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance,privateKey,setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+
+    const address = toHex(secp256k1.getPublicKey(privateKey))
+
+    console.log('address', address)
+
+    setAddress(address)
+
     if (address) {
       const {
         data: { balance },
@@ -19,9 +29,13 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type in a private key" value={privateKey} onChange={onChange}></input>
       </label>
+
+      <div>
+        Address: {address}
+      </div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
